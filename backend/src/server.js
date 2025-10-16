d729840b6888894da008bc289e1c2c80ebede2cc
IsSact <aisaachung@gmail.com>
2025-10-16 13:24:09 -0400

    move server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
-import app from "./src/app.js";
-import config from "./src/config/index.js";
+import app from "./app.js";
+import config from "./config/index.js";
 
 const PORT = config.port || 3000;
 
